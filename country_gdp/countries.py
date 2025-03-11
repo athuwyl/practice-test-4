1ad05b54a161659097d44e25bd5567d346a026de
Mary Vo <125909623+athuwyl@users.noreply.github.com>
2025-03-11 13:14:42 -0700

    Update countries.py

--- country_gdp/countries.py
+++ country_gdp/countries.py
@@ -3,3 +3,13 @@ import pandas
 df = pandas.read_csv("country_gdp.csv")
 df['population'] = df['population'].apply(lambda x: int(x.replace(",", "")))
 
+def developed_countries(df: pd.DataFrame) -> pd.DataFrame:
+  developed = df[(df['gdp_per_cap'] > '12000') & (df['unemployment_rate'] <= '7.5%')]
+  return developed
+
+  df.drop(number, gov_type)
+  
+  total_gdp = df[(df['gdp_per_cap']) * df[('population')]]
+  
+  
+
